b1d991bf5fcc07757354f94c76cca0e87abcd8cd
SnehaMgithub <snehamathes2000@gmail.com>
2024-09-29 09:49:52 +0530

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({openModal,cartCount}) => {
     
     return (
            <nav className='bg-slate-500 text-white flex p-3 justify-between'>
-           <h2 className='text-2xl p-2 font-bold'>FakeStore</h2>
+           <h2 className='text-2xl p-2 font-bold'>ʍʏֆɦօք</h2>
            <button className="relative p-2 text-2xl font-bold" onClick={openModal}>Cart {cartCount}</button>
            </nav>
     );
